a3832e10c07d513258929c61394bb6e0733e4545
Marcio Carolino <marciocarolino@gmail.com>
2026-04-12 22:25:27 -0300

    feat: userService

--- src/app/source/user/user.service.ts
+++ src/app/source/user/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  async findUsers(): Promise<any> {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findUsers(): Promise<any> {
+    return this.prisma.user.findMany();
+  }
 }
