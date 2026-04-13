1a6b869913f376f234dc60bd076bd33d1c964be3
Marcio Carolino <marciocarolino@gmail.com>
2026-04-12 22:40:37 -0300

    feat: create user

--- src/app/source/user/user.controller.ts
+++ src/app/source/user/user.controller.ts
@@ -1,7 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  async findUserActived(): Promise<CreateUserDTO[]> {
+    return this.userService.findUsersActived();
+  }
 }
