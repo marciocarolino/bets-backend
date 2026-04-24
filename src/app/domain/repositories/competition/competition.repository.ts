54740af6519f609517f33f95cf504259c074ad30
Stharley Maxwell <stharleymax@gmail.com>
2026-04-24 17:38:57 -0300

    feat(competition): add DomainEvent interface and update save method to handle events

--- src/app/domain/repositories/competition/competition.repository.ts
+++ src/app/domain/repositories/competition/competition.repository.ts
@@ -1,6 +1,8 @@
 import { IRepository } from '../base.repository';
 import { Competition } from '../../entities/competition/competition.entity';
 
+export const COMPETITION_REPOSITORY = 'COMPETITION_REPOSITORY';
+
 export interface CompetitionCriteria {
   name?: string;
   slug?: string;
