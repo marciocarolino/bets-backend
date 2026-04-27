e18a950b29ddd5d815b8ed2cd78ed76e6d6aa8c4
Marcio Carolino <marciocarolino@gmail.com>
2026-04-27 10:26:01 -0300

    feat: conflict

--- src/app/domain/repositories/user/IUser-repository.ts
+++ src/app/domain/repositories/user/IUser-repository.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import { UserEntity } from '../../entities/user/user.entity';
-import { CreateUserData } from './Icreate-user.data';
-import { IFindUserEmailData } from './Ifind-user-email.data';
-import { UpdateUserData } from './update-user.data';
-=======
 import { UserEntity } from "../../entities/user/user.entity";
 import { CreateUserData } from "./Icreate-user.data";
 import { IFindUserEmailData } from "./Ifind-user-email.data";
->>>>>>> e94be6266d4452547110615f3930e749e6adbf35
 
 export interface IUserRepository {
   findAll(): Promise<UserEntity[]>;
@@ -16,5 +9,5 @@ export interface IUserRepository {
 
   create(user: CreateUserData): Promise<UserEntity>;
 
-  update(user:  UserEntity): Promise<UserEntity>;
+  update(user: UserEntity): Promise<UserEntity>;
 }
